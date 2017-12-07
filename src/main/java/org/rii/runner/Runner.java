5d5884da57f79e0a0c6edf205203903c9b7886b6
Sergey Ripa <snripa@gmail.com>
2017-12-07 20:40:03 +0200

    init

---
+++ src/main/java/org/rii/runner/Runner.java
@@ -0,0 +1,8 @@
+package org.rii.runner;
+
+public class Runner {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/org/rii/service/TestService.java
@@ -0,0 +1,7 @@
+package org.rii.service;
+
+import java.util.List;
+
+public interface TestService {
+    List<String> getList(String[] arr);
+}
